3d8b5c3acc7fcdac552ac6929d8e5a8e1c86bf1e
jhon perez <jhonperez.j95@gmail.com>
2018-09-15 12:22:39 -0500

    Final

---
+++ Curso_Programacion_Base/canvas/dibujo.js
@@ -0,0 +1,76 @@
+var d = document.getElementById("idDibujo");
+var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf, xi, yf;
+/*
+while (l < lineas) {
+    yi = 10 * l;
+    xf = 10 * (l + 1)
+    dibujarLinea("red", 0, yi, xf, 300);
+    l = l + 1;
+
+}
+
+for ( l = 0; l < lineas; l++) {
+    yi = 10 * l;
+    xf = 10 * (l + 1)
+    dibujarLinea("red", 0, yi, xf, 300);
+    
+}
+for ( l = 0; l < lineas; l++) {
+    xi = 10 * l;
+    xf = 10 * (l+1);
+    dibujarLinea("blue", xi, 0, 300, xf );
+} 
+for (let l = 0; l < lineas; l++) {
+    xi = 10 * l;
+    xf = 10 * (l+1);
+    yi = 10 * l;
+    dibujarLinea("red", 0, yi, xf, 300);
+    dibujarLinea("blue", xi, 0, 300, xf );
+}
+
+for (let l = 0; l < lineas; l++) {
+    yi = 10*l;
+    xf = 300 - (l*10) ;
+    dibujarLinea("yellow", 0, yi, xf, 0 );
+}
+for (let l = 0; l < lineas; l++) {
+   xi = 10 *l;
+   yf = 300 - (l*10);
+    dibujarLinea("green", xi, 300, 300, yf );
+} 
+
+for (let l = 0; l < lineas; l++) {
+    xi = 10 *l;
+    yf = 300 - (l*10);
+    yi = 10*l;
+    xf = 300 - (l*10) ;
+    dibujarLinea("yellow", 0, yi, xf, 0 );
+    dibujarLinea("green", xi, 300, 300, yf );
+}
+*/
+for ( l = 0; l < lineas; l++) {
+    xi = 10 * l;
+    xf = 10 * (l+1);
+    yi = 10 * l;
+    yf = 300 - (l*10) ;
+    dibujarLinea("red", 0, yi, xf, 300);
+    dibujarLinea("blue", xi, 0, 300, xf );
+    dibujarLinea("yellow", 0, yi, 300-xf, 0 );
+    dibujarLinea("green", xi, 300, 300, yf );
+}
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, ) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xInicial, yInicial);
+    lienzo.lineTo(xFinal, yFinal);
+    lienzo.stroke();
+    lienzo.closePath();   
+}
+
+// dibujarLinea("green", 10, 300 ,300, 290);
+// dibujarLinea("red", 20, 300, 300, 280);
+
+
