05dcbef89ea0f7a9ca8f5584bf3bcf5361f6106f
JhonPerez95 <jhonperez.j95@gmail.com>
2019-02-19 22:47:34 -0500

    Terminando MongoDB

--- MongoDB/mongodb.js
+++ MongoDB/mongodb.js
@@ -123,11 +123,94 @@ db.Clientes.find(
     }
 );
 
-//Buscar datos condicional mayor que , lt
+//Buscar datos condicional menor que , lt
 db.Clientes.find(
     {age:
         {$lt: 25}
     }
 );
 
-//
+// ambos al tiempo , lt , gt
+db.Clientes.find(
+    {age:
+        {$lt: 30, $gt:20}
+    }
+);
+
+//Insertar datos en lo objetos ingresados
+db.Clientes.insert(
+    {
+        firstName: "Pedro",
+        lastName: "Santana",
+        addres: {
+            city: "London"
+        },
+        age: 29,
+        sex: "Man"
+    }
+);
+
+// Para buscar este tipo de datos seria;
+db.Clientes.find(
+    {"addres.city": "London"} //Con formato json se busca
+);
+
+//Buscar datos que contengan x cosa 
+db.Clientes.find(
+    {
+        firstName:{$regex:"dro"}
+    }
+);
+
+//Ordenar en la busqueda con el metodo sort() ,  
+// 1 de menor  a mayor A-Z , inversa seria -1
+db.Clientes.find().sort(
+    {
+        lastName: 1
+    }
+);
+
+//Contador de registros, metodo count
+db.Clientes.count();
+
+//Combina meotdos de busqueda y lo cuenta count
+db.Clientes.find( {age: {$gt:20}}).count();
+
+// Tambien se puede limitar las busquedas con el metodo , limit
+db.Clientes.find().limit(5);
+
+//Combinando funciones de javaScript
+db.Clientes.find().forEach(
+    function(doc) {
+        print("Primer Nombre: " + doc.firstName);
+    }
+);
+
+
+//Documentacion de MongoDB
+db . inventario . insertMany ([ 
+    // MongoDB agrega el campo _id con un ObjectId si _id no está presente 
+    { 
+        elemento :  "diario" ,
+        cantidad :  25 , 
+        estado :  "A" , 
+        tamaño :  {  h :  14 ,  w :  21 ,  uom unt();:  "cm "  }, 
+        tags :  [  " blank " ,  " red "  ]  
+    }, 
+    {  
+        item :  " notebook " ,
+        qty : 50 , 
+        estado :  "A" , 
+        tamaño :  {  h :  8.5 ,  w :  11 ,  uom :  "en"  },
+        etiquetas :  [  "rojo" ,  "en blanco"  ]  
+    }, 
+    {  elemento :  "papel" ,  cantidad :  100 ,  estado :  "D" , 
+        tamaño :  {  h :  8.5 ,  w :  11 ,  uom :  "en" },  tags :  [  "red" ,  "blank" ,  "plain"  ]  }, 
+    {  item :  "planner" ,  qty :  75 ,  status :  "D" , 
+        tamaño :  {  h :  22.85 ,  w :  30 ,  uom :  " cm "  },  tags :  [  " blank " ,  " red "  ]  }, 
+    {  
+        item :  " postcard " , cantidad :  45,  estado :  "A" , 
+        tamaño :  {  h :  10 ,  w :  15.25 ,  uom :  "cm"  },
+        etiquetas :  [  "azul"  ]  
+    } 
+ ]);
