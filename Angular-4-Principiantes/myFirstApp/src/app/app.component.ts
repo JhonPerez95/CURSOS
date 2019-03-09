aa7900e2a5e38a44090a9f4046b601942b7710e8
JhonPerez95 <jhonperez.j95@gmail.com>
2019-03-08 23:59:43 -0500

    angular inicios

--- Angular-4-Principiantes/myFirstApp/src/app/app.component.ts
+++ Angular-4-Principiantes/myFirstApp/src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Mi Primera App ';
+export class AppComponent { // Declaramos el tipo de dato de cada variable TypeScript
+  title: string;
+  email: string;
+  webSite: string;
+  hobbies: string[];
+  estado: boolean;
+
+  constructor(){
+    this.title = 'Mi Primera App ';
+    this.email = 'jhonperez.j95@gmial.com';
+    this.webSite = 'https://www.facebook.com/JhonPerez95';
+    this.hobbies = ['Hacer ejercicio', 'Estudiar', 'Ir al cine'];
+    this.estado = false;
+  }
+  mostrarHobbies(){
+    this.estado = !this.estado; //Cambie el estado al inverso
+  }
 }
+
